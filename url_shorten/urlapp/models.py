a5f82da7e32811092a735c6ad071d4fb34a86e01
aqdas1234 <bsef21m505@pucit.edu.pk>
2025-07-12 16:35:35 +0500

    created model of ShortURL

--- url_shorten/urlapp/models.py
+++ url_shorten/urlapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+def generate_shortcode(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+class ShortURL(models.Model):
+    url = models.URLField()
+    shortCode = models.CharField(max_length=10, unique=True)
+    accessCount = models.PositiveIntegerField(default=0)  
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.shortCode} -> {self.url}"
