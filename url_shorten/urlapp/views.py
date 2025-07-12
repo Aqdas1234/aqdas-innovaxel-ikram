d8dea062fd79c0f8f1ff47c8a09bb8543274439c
aqdas1234 <bsef21m505@pucit.edu.pk>
2025-07-12 16:43:31 +0500

    created shortenurl post api

--- url_shorten/urlapp/views.py
+++ url_shorten/urlapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ShortURLSerializer
 
-# Create your views here.
+class ShortenURLView(APIView):
+    def post(self, request):
+        serializer = ShortURLSerializer(data=request.data)
+        if serializer.is_valid():
+            short_url = serializer.save()  
+            return Response(ShortURLSerializer(short_url).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
