804b704443865c7fce679b1d063e6684267a4dc7
aqdas1234 <bsef21m505@pucit.edu.pk>
2025-07-12 16:57:42 +0500

    implement GET API to retrieve original URL from short URL

--- url_shorten/urlapp/urls.py
+++ url_shorten/urlapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ShortenURLView
+from .views import ShortenURLView, ShortURLDetailView
 
 urlpatterns = [
     path('shorten/', ShortenURLView.as_view(), name='shorten-url'),
+      path('shorten/<str:shortCode>', ShortURLDetailView.as_view(), name='short-url-detail'),
 ]
--- url_shorten/urlapp/views.py
+++ url_shorten/urlapp/views.py
@@ -2,6 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ShortURLSerializer
+from rest_framework.generics import RetrieveAPIView
+from .models import ShortURL
+from django.shortcuts import get_object_or_404
+
 
 class ShortenURLView(APIView):
     def post(self, request):
@@ -10,3 +14,9 @@ class ShortenURLView(APIView):
             short_url = serializer.save()  
             return Response(ShortURLSerializer(short_url).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ShortURLDetailView(RetrieveAPIView):
+    def get(self, request, shortCode):
+        short_url = get_object_or_404(ShortURL, shortCode=shortCode)
+        serializer = ShortURLSerializer(short_url)
+        return Response(serializer.data, status=status.HTTP_200_OK)
